1410bd69fb964afaddae684906b7cfc23331dbdc
DavidC <davidcardenasgonz@gmail.com>
2024-05-10 10:34:32 -0600

    Set color variables for the Navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,11 @@ export default {
         "primary-bg": "#000000", // "black"
         "secondary-bg": "#121212",
         "component-bg": "#242424",
+        "contrast-bg": "white",
         "primary-text": "#FFFFFF", // "white"
         "progress-bar": "rgb(63 98 18)",
         "controls-bg": "rgb(248 250 252)",
+        "profile-icon": "rgb(168 85 247)",
       },
     },
   },
