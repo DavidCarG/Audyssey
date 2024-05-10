f38deebc0ae8dd09e135e964cf8e731eef548396
DavidC <davidcardenasgonz@gmail.com>
2024-05-10 11:33:26 -0600

    Add navigation functionality to AlbumItem component

--- src/components/AlbumItem.jsx
+++ src/components/AlbumItem.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const AlbumItem = ({ image, name, desc, id }) => {
+  const navigate = useNavigate();
+
   return (
-    <div className="min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]">
+    <div
+      onClick={() => navigate(`/album/${id}`)}
+      className="min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-album-hover"
+    >
       <img className="rounded" src={image} alt="" />
       <p className="font-bold mt-2 mb-1">{name}</p>
       <p className="text-album-desc text-sm">{desc}</p>
