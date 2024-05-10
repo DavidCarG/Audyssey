65485d2dfbac5e54811380426b39025ae805cb43
DavidC <davidcardenasgonz@gmail.com>
2024-05-10 17:26:40 -0600

    Update colors in tailwind.config.js for primary and secondary backgrounds, and component background

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,16 +4,17 @@ export default {
   theme: {
     extend: {
       colors: {
-        "primary-bg": "#000000", // "black"
-        "secondary-bg": "#121212",
-        "component-bg": "#242424",
+        "primary-bg": "#FFFFFF", // "black"
+        "secondary-bg": "#10537d",
+        "component-bg": "#B7D1F9",
         "contrast-bg": "white",
         "primary-text": "#FFFFFF", // "white"
-        "progress-bar": "rgb(63 98 18)",
+        "secondary-text": "#000000", // "black"
+        "progress-bar": "#647680",
         "controls-bg": "rgb(248 250 252)",
         "profile-icon": "rgb(168 85 247)",
         "album-hover": "#ffffff26",
-        "album-desc": "rgb(226 232 240)",
+        "album-desc": "#0D3D5A",
         "playlist-info": "#a7a7a7",
         "song-hover": "#ffffff26",
       },
