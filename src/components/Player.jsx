3533f1341307c072e235cf52d6c4fcb9862006f2
DavidC <davidcardenasgonz@gmail.com>
2024-05-10 13:22:32 -0600

    Refactor Player component to display track time with leading zero for seconds

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -44,7 +44,8 @@ const Player = () => {
         </div>
         <div className="flex items-center gap-5">
           <p>
-            {time.currentTime.minute}:{time.currentTime.second}
+            {time.currentTime.minute}:
+            {String(time.currentTime.second).padStart(2, "0")}
           </p>
           <div
             ref={seekBg}
@@ -56,7 +57,8 @@ const Player = () => {
             />
           </div>
           <p>
-            {time.totalTime.minute}:{time.totalTime.second}
+            {time.totalTime.minute}:
+            {String(time.totalTime.second).padStart(2, "0")}
           </p>
         </div>
       </div>
