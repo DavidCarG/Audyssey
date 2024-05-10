7f555f47ac85245b02c01bbc36e0f5fec4fe7eba
DavidC <davidcardenasgonz@gmail.com>
2024-05-10 14:31:08 -0600

    Add playWithId function to SongItem component for playing a specific track

--- src/components/SongItem.jsx
+++ src/components/SongItem.jsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
+import { PlayerContext } from "../context/PlayerContext";
 
 const SongItem = ({ name, image, desc, id }) => {
+  const { playWithId } = useContext(PlayerContext);
+
   return (
-    <div className="min-w-[180px] p-2 px-3 ronded cursor-pointer hover:bg-album-hover">
+    <div
+      onClick={() => playWithId(id)}
+      className="min-w-[180px] p-2 px-3 ronded cursor-pointer hover:bg-album-hover"
+    >
       <img className="rounded" src={image} alt="" />
       <p className="font-bold mt-2 mb-1">{name}</p>
       <p className="text-album-desc text-sm">{desc}</p>
