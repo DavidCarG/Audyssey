61a049b79afb6829651754532c271f08908576d0
DavidC <davidcardenasgonz@gmail.com>
2024-05-10 11:33:36 -0600

    Add route for displaying album details in Display component

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import DisplayAlbum from "./DisplayAlbum";
 import DisplayHome from "./DisplayHome";
 
 const Display = () => {
@@ -7,6 +8,7 @@ const Display = () => {
     <div className="w-[100%] m-2 px-6 pt-4 rounded bg-secondary-bg text-primary-text overflow-auto lg:w-[75%] lg:ml-0">
       <Routes>
         <Route path="/" element={<DisplayHome />} />
+        <Route path="/album/:id" element={<DisplayAlbum />} />
       </Routes>
     </div>
   );
