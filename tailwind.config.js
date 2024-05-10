d57f40bf80651c2508fa5a7c1d13f5333e805c48
DavidC <davidcardenasgonz@gmail.com>
2024-05-10 09:17:35 -0600

    Set some tailwind color variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-bg": "#000000", // "black"
+        "secondary-bg": "#121212",
+        "component-bg": "#242424",
+        "primary-text": "#FFFFFF", // "white"
+      },
+    },
   },
   plugins: [],
 };
