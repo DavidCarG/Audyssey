47768a51283775928092bff4077cfa92ffba8781
DavidC <davidcardenasgonz@gmail.com>
2024-05-10 17:28:02 -0600

    Update Sidebar component to use secondary text color and secondary background color

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,25 +6,25 @@ const Sidebar = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="w-[25%] h-full p-2 flex-col gap-2 text-primary-text hidden lg:flex">
+    <div className="w-[25%] h-full p-2 flex-col gap-2 text-secondary-text hidden lg:flex">
       <div className="bg-secondary-bg h-[15%] rounded flex flex-col justify-around">
         <div
           onClick={() => navigate("/")}
           className="flex items-center gap-3 pl-8 cursor-pointer"
         >
           <img className="w-6" src={assets.home_icon} alt="" />
-          <p className="font-bold">Home</p>
+          <p className="font-bold text-component-bg">Home</p>
         </div>
         <div className="flex items-center gap-3 pl-8 cursor-pointer">
           <img className="w-6" src={assets.search_icon} alt="" />
-          <p className="font-bold">Search</p>
+          <p className="font-bold text-component-bg">Search</p>
         </div>
       </div>
       <div className="bg-secondary-bg h-[85%] rounded">
         <div className="p-4 flex items-center justify-between">
           <div className="flex items-center gap-3">
             <img className="w-8" src={assets.stack_icon} alt="" />
-            <p className="font-semibold">Your Library</p>
+            <p className="font-semibold text-component-bg">Your Library</p>
           </div>
           <div className="flex items-center gap-3">
             <img className="w-5" src={assets.arrow_icon} alt="" />
@@ -34,14 +34,14 @@ const Sidebar = () => {
         <div className="p-4 bg-component-bg m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4">
           <h1>Create your first playlist</h1>
           <p className="font-light">it's easy we will help you</p>
-          <button className="px-4 py-1.5 bg-contrast-bg text-[15px] text-primary-bg rounded-full mt-4">
+          <button className="px-4 py-1.5 bg-contrast-bg text-[15px] text-secondary-text rounded-full mt-4">
             Create Playlist
           </button>
         </div>
         <div className="p-4 bg-component-bg m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4 mt-4">
           <h1>Let's find some podcast to follow!</h1>
           <p className="font-light">We'll keep you updated on new episodes</p>
-          <button className="px-4 py-1.5 bg-contrast-bg text-[15px] text-primary-bg rounded-full mt-4">
+          <button className="px-4 py-1.5 bg-contrast-bg text-[15px] text-secondary-text rounded-full mt-4">
             Browse podcasts
           </button>
         </div>
