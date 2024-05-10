ccb3a67a5a54cb5a30b95062fa500b4c0495441b
DavidC <davidcardenasgonz@gmail.com>
2024-05-10 09:25:27 -0600

    Create initial version of the player

---
+++ src/components/Player.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { songsData } from "../assets/assets";
+
+const Player = () => {
+  return (
+    <div className="h-[10%] bg-primary-bg flex justify-between items-center text-primary-text px-4">
+      <div className="hidden lg:flex items-center gap-4">
+        <img className="w-12" src={songsData[0].image} alt="" />
+        <div>
+          <p>{songsData[0].name}</p>
+          <p>{songsData[0].desc}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Player;
